3cce184caf3e98fc869e7232f6404e7a87ffe3db
Rushil Singh <rushilsingh@gmail.com>
2019-08-16 18:31:17 +0530

    Cleaned up stray code snippet

--- newsanalysisherokuapp/views.py
+++ newsanalysisherokuapp/views.py
@@ -2,10 +2,6 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from fuzzy_search import DataSet
-import redis
-import os
-d = DataSet()
 
 def index(request):
     return render(request, 'base.html', {'data': {}} )
