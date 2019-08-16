dc6c3bbca5b5e7135ffa765b73a46b3800774079
Rushil Singh <rushilsingh@gmail.com>
2019-08-16 18:52:29 +0530

    Setting up templates

--- newsanalysisapp/settings.py
+++ newsanalysisapp/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'newsanalysisapp.urls'
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -120,5 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT =    os.path.join(PROJECT_DIR, "staticfiles")
