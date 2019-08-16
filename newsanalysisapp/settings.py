58b20346d7e7d199e35c4a71ec57c863ad05eed7
Rushil Singh <rushilsingh@gmail.com>
2019-08-16 18:50:38 +0530

    Setting up templates

--- newsanalysisapp/settings.py
+++ newsanalysisapp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'newsanalysisapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': os.path.join(PROJECT_DIR, "templates"),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
